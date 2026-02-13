bce96f71fb3b0dd26be00c42638b7d7c179bcd41
Jace <jt.scripter@gmail.com>
2026-02-13 11:32:46 +0100

    presentation script updated_at cast to human readable

--- app/Models/PresentationScript.php
+++ app/Models/PresentationScript.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
 class PresentationScript extends Model
 {
@@ -13,5 +15,12 @@ class PresentationScript extends Model
     public function presentation(): HasMany
     {
         return $this->hasMany(Presentation::class);
+    }
+
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Carbon::parse($value)->diffForHumans(),
+        );
     }
 }
