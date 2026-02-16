f92d32289b377e4da48a35acc12e2513b1c5b6e4
Jace <jt.scripter@gmail.com>
2026-02-16 15:52:15 +0100

    cast presentation script from md to html

--- app/Models/PresentationScript.php
+++ app/Models/PresentationScript.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use TuchSoft\CommonMarkHeadingShifter\HeadingShifterExtension;
 
 class PresentationScript extends Model
 {
@@ -15,6 +17,16 @@ class PresentationScript extends Model
     public function presentation(): HasMany
     {
         return $this->hasMany(Presentation::class);
+    }
+
+    protected function content(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Str::of($value)->markdown(
+                ['heading_shifter' => ['shift_by' => 1]],
+                [new HeadingShifterExtension]
+            ),
+        );
     }
 
     protected function updatedAt(): Attribute
