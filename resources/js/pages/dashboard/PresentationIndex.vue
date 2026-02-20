06d47c2838c88b6e5e99f60995fd2d07aa6500bd
Jace <jt.scripter@gmail.com>
2026-02-20 14:50:48 +0100

    added search to presentation index

--- resources/js/pages/dashboard/PresentationIndex.vue
+++ resources/js/pages/dashboard/PresentationIndex.vue
@@ -8,6 +8,7 @@ import { Container } from '@/components/dashboard/containers';
 import IndexLink from '@/components/dashboard/models/IndexLink.vue';
 import NewLink from '@/components/dashboard/models/NewLink.vue';
 import NewPresentation from '@/components/dashboard/forms/NewPresentation.vue';
+import { Search } from '@/components/global/form';
 
 defineProps<{
     allPresentations: Presentation[];
@@ -24,37 +25,40 @@ const breadcrumbs: BreadcrumbItem[] = [{
         <Container
             id="presentation-index"
             title="Full Overview"
-            class="grid auto-rows-min gap-4 md:grid-cols-3"
+            class="space-y-10"
         >
-            <NewLink title="Presentation">
-                <h2 class="text-lg font-semibold my-5"> Create a new Presentation </h2>
-                <NewPresentation/>
-            </NewLink>
-            <template v-for="_presentation in allPresentations">
-                <IndexLink
-                    :title="_presentation.title"
-                    :semantic-title="true"
-                    :link="admin_presentations(_presentation.id)"
-                    :last-edit="_presentation.updated_at"
-                >
-                    <span
-                        aria-hidden="true"
-                        class="
-                            pointer-events-none
-                            absolute top-0 right-4 z-10
-                            block px-2 border rounded-sm
-                            -translate-y-1/2
-                            text-sm italic
-                            bg-white
-                            "
+            <Search />
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <NewLink title="Presentation">
+                    <h2 class="text-lg font-semibold my-5"> Create a new Presentation </h2>
+                    <NewPresentation/>
+                </NewLink>
+                <template v-for="_presentation in allPresentations">
+                    <IndexLink
+                        :title="_presentation.title"
+                        class="search-indexable"
+                        :link="admin_presentations(_presentation.id)"
+                        :last-edit="_presentation.updated_at"
                     >
-                        {{ _presentation.presentation_visibility.name }}
-                    </span>
-                    <span class="block mt-auto" aria-label="A presentation">
-                        by: {{ _presentation.user.name }}
-                    </span>
-                </IndexLink>
-            </template>
+                        <span
+                            aria-hidden="true"
+                            class="
+                                pointer-events-none
+                                absolute top-0 right-4 z-10
+                                block px-2 border rounded-sm
+                                -translate-y-1/2
+                                text-sm italic
+                                bg-white
+                                "
+                        >
+                            {{ _presentation.presentation_visibility.name }}
+                        </span>
+                        <span class="block mt-auto" aria-label="A presentation">
+                            by: {{ _presentation.user.name }}
+                        </span>
+                    </IndexLink>
+                </template>
+            </div>
         </Container>
     </AppLayout>
 </template>
