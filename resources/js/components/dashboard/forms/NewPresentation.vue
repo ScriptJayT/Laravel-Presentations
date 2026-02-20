246821f37953e8b5c2fbc014d57ed28c4f0f8706
Jace <jt.scripter@gmail.com>
2026-02-20 09:46:43 +0100

    refactord form wrapper to better build up

--- resources/js/components/dashboard/forms/NewPresentation.vue
+++ resources/js/components/dashboard/forms/NewPresentation.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import FormField from '@/components/global/form/FormField.vue';
-import { Save } from 'lucide-vue-next';
 
 import { store } from '@/routes/admin_presentation';
 import { Form } from '@/components/global/form';
@@ -8,14 +7,11 @@ import { Form } from '@/components/global/form';
 
 <template>
     <Form
-        id="update-script-form"
-        class="
-            relative
-            grid gap-6
-            inert:opacity-50
-            "
-        :send-to="store.form()"
+        class="relative space-y-6"
         form-action="create"
+        :send-to="store.form()"
+        show-button
+        button-text="Create new Presentation"
         success-message="New Presentation was created"
         v-slot="{ errors }"
     >
@@ -28,26 +24,10 @@ import { Form } from '@/components/global/form';
                 class="grow outline-none"
             >
         </FormField>
-
-        <label class="cursor-pointer flex gap-2 items-center">
-            <span class="select-none">Directly go to edit-page</span>
-            <input type="checkbox" name="goto" checked>
-        </label>
-
-        <div class="flex gap-5 items-end">
-            <button
-                class="
-                    cursor-pointer select-none
-                    flex items-center gap-1
-                    w-fit
-                    px-3 py-2 ml-auto
-                    border border-green-700 rounded-md
-                    outline-offset-8
-                    "
-            >
-                <span> Create new Presentation </span>
-                <Save class="size-4"/>
-            </button>
-        </div>
+        <FormField
+            id="continue-on" label="Continue to edit-page:"
+        >
+            <input type="checkbox" name="goto" checked id="continue-on">
+        </FormField>
     </Form>
 </template>
--- resources/js/components/global/form/Form.vue
+++ resources/js/components/global/form/Form.vue
@@ -1,18 +1,37 @@
 <script setup lang="ts">
 import { RouteFormDefinition } from '@/wayfinder';
 import { Form } from '@inertiajs/vue3';
+import { Save, Trash } from 'lucide-vue-next';
+
 
 import { UnsavedChanges, ProcessIndicator } from '@/components/global/form'
+import { FunctionalComponent } from 'vue';
 
-const props = defineProps<{
-    sendTo: RouteFormDefinition<'post'>;
-    id?: string;
-    class?: string;
-    formAction?: "edit" | "create";
-    successMessage?: string;
-    onError?: (_arg: unknown) => void;
-    onSuccess?: (_arg: unknown) => void;
-}>();
+const props = withDefaults(
+    defineProps<{
+        sendTo: RouteFormDefinition<'post'>;
+        id?: string;
+        class?: string;
+        formAction?: "edit" | "create" | "delete";
+        successMessage?: string;
+        showButton?: boolean;
+        buttonText?: string;
+        buttonIcon?: FunctionalComponent;
+        onError?: (_arg: unknown) => void;
+        onSuccess?: (_arg: unknown) => void;
+    }>(),
+    {
+        showButton: false,
+        buttonText: "Submit",
+    }
+);
+const icon = props.buttonIcon ?? (
+    (props.formAction === "create") || (props.formAction === "edit")
+    ? Save
+    : props.formAction === "delete"
+        ? Trash
+        : null
+);
 
 function success(_response: unknown) {
     if(props.onSuccess) props.onSuccess(_response);
@@ -37,15 +56,42 @@ function fail(_response: unknown) {
         disable-while-processing
         v-slot="{ errors, processing, isDirty, wasSuccessful, hasErrors }"
     >
-        <div class="form--meta | absolute bottom-full right-0 m-0">
-            <ProcessIndicator :is-in-process="processing"/>
-            <UnsavedChanges
-                v-if="formAction==='edit'"
-                :has-unsaved-changes="isDirty && !processing"
-            />
-        </div>
+        <template v-if="formAction==='edit'">
+            <div class="form--meta | absolute bottom-full right-0 m-0">
+                <ProcessIndicator :is-in-process="processing"/>
+                <UnsavedChanges :has-unsaved-changes="isDirty && !processing" />
+            </div>
+        </template>
 
         <slot :hasErrors :errors :wasSuccessful />
+
+        <template v-if="showButton">
+            <button
+                type="submit"
+                :class="
+                    formAction === 'create' ? 'border-green-700' : '' +
+                    formAction === 'edit' ? 'border-cyan-700' : '' +
+                    formAction === 'delete' ? 'border-red-700' : ''
+                    "
+                class="
+                    form-submit--button |
+                    cursor-pointer select-none
+                    flex items-center gap-2
+                    w-fit px-3 py-2
+                    border rounded-md mx-auto
+                    outline-offset-8
+                    transition-[width]
+                    "
+            >
+                <span>
+                    <ProcessIndicator :is-in-process="processing"/>
+                </span>
+                <span> {{ buttonText }} </span>
+                <span class="form-submit--icon">
+                    <component v-if="icon" :is="icon" class="size-4" />
+                </span>
+            </button>
+        </template>
 
         <template v-if="successMessage">
             <div v-show="wasSuccessful" aria-live="polite">
