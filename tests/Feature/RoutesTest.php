8643a30a159eb65da2c0c9ff5b88da7bd7a928d4
Jace <jt.scripter@gmail.com>
2026-02-26 10:05:07 +0100

    added more routes to test

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -2,12 +2,19 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class RoutesTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_app_is_live()
+    {
+        $response = $this->get('/up');
+        $response->assertOk();
+    }
 
     public function test_homepage_is_accessible()
     {
@@ -23,5 +30,43 @@ class RoutesTest extends TestCase
         $response->assertRedirect();
         $response = $this->get('/scripts');
         $response->assertRedirect();
+    }
+
+    public function test_dashboardpages_are_inaccessible_when_guest()
+    {
+        $response = $this->get('/dashboard/presentations');
+        $response->assertRedirect();
+        $response = $this->get('/dashboard/scripts');
+        $response->assertRedirect();
+        $response = $this->get('/dashboard/users');
+        $response->assertRedirect();
+    }
+
+    public function test_adminpages_are_accessible_when_user()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->get('/dashboard/presentations');
+        $response->assertOk();
+        $response = $this->get('/dashboard/scripts');
+        $response->assertOk();
+        $response = $this->get('/dashboard/users');
+        $response->assertOk();
+    }
+
+    public function test_profilepages_are_inaccessible_when_guest()
+    {
+        $response = $this->get('/settings/profile');
+        $response->assertRedirect();
+    }
+
+    public function test_profilepages_are_accessible_when_user()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->get('/settings/profile');
+        $response->assertOk();
     }
 }
