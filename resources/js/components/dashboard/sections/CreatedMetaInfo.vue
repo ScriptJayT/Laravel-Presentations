8a758ac2dfb4955647cbc0b30fbb7ecba181508e
Jace <jt.scripter@gmail.com>
2026-03-02 13:59:51 +0100

    added user link to meta info

--- resources/js/components/dashboard/sections/CreatedMetaInfo.vue
+++ resources/js/components/dashboard/sections/CreatedMetaInfo.vue
@@ -2,6 +2,7 @@
 import type { Model, User } from '@/types';
 import { ExternalLink } from 'lucide-vue-next';
 import { RouteDefinition } from '@/wayfinder';
+import { admin_users } from "@/routes";
 const props = defineProps<{
     model: { user?: User } & Model;
     previewUrl?: RouteDefinition<'get'>;
@@ -27,9 +28,20 @@ const props = defineProps<{
         <hr>
     </template>
     <template v-if="model.user">
-        <span class="block">
-            Created by: {{ model.user.name }}
-        </span>
+        <a
+            :href="admin_users(model.user.id).url"
+            target="_blank"
+            class="
+                cursor-pointer select-none
+                flex justify-between gap-1
+                rounded-sm
+                outline-sidebar-ring
+                outline-offset-8
+                "
+        >
+            <span> Created by: {{ model.user.name }} </span>
+            <span aria-hidden="true"> <ExternalLink class="size-4" /> </span>
+        </a>
         <span class="block">
             on: {{ model.created_at }}
         </span>
