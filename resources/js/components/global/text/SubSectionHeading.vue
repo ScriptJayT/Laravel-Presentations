b406e58778644794027c782f0fb348377297f55c
Jace <jt.scripter@gmail.com>
2026-03-09 13:32:07 +0100

    cleanup SubSectionHeading

--- resources/js/components/global/text/SubSectionHeading.vue
+++ resources/js/components/global/text/SubSectionHeading.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-withDefaults(
-    defineProps<{
-        title: string;
-        description?: string;
-    }>(),
-    {}
-);
+defineProps<{
+    title: string;
+    description?: string;
+    class?: string;
+}>();
 </script>
 
 <template>
-    <header data-component="global/text/SubSectionHeading" >
+    <header
+        data-component="global/text/SubSectionHeading"
+        :class
+    >
         <h3 class="min-h-lh text-lg font-semibold">
             {{ title }}
         </h3>
-        <p v-if="description" class="text-sm text-muted-foreground">
+        <p
+            v-if="description"
+            class="text-sm text-muted-foreground"
+        >
             {{ description }}
         </p>
     </header>
