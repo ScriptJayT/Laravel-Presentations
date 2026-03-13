8766d6e72a6fbbf99c686e9d601f1f78251141a9
Jace <jt.scripter@gmail.com>
2026-03-13 13:13:51 +0100

    cleanup

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -68,7 +68,7 @@ export function plural(countable: Countable, word: string, plural_addition: stri
 }
 
 function getAppProp(_prop: keyof AppServiceShared) {
-    const props = (usePage().props as unknown as AppServiceShared);
+    const props = usePage().props;
     return props[_prop];
 }
 export function getAppEnums() {
@@ -78,8 +78,7 @@ export function getAppName() {
     return getAppProp("appName") as AppServiceShared['appName'];
 }
 export function getUser(_reactive: boolean) {
-    const page = usePage();
-    const auth = page.props.auth as Auth;
+    const auth = usePage().props.auth as Auth;
     if(!auth) return null;
     if(_reactive) return computed(() => auth.user ?? null);
     return auth.user ?? null;
