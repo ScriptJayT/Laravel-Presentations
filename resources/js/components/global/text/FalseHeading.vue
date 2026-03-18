00610f4be2eb1c5153b79e760ee6fd8c36fd283e
Jace <jt.scripter@gmail.com>
2026-03-18 11:36:32 +0100

    pass class to headin too in falseheading

--- resources/js/components/global/text/FalseHeading.vue
+++ resources/js/components/global/text/FalseHeading.vue
@@ -1,32 +1,26 @@
 <script setup lang="ts">
-import {
-    SectionHeading,
-    SubSectionHeading,
-    SubSectionHeadingClasses,
-} from '.';
+import { SectionHeading, SubSectionHeading } from '.';
 withDefaults(
     defineProps<{
         title: string;
         description?: string;
         class?: string;
-        semanticLvl?: 2|3;
-        visualLvl?: 2|3;
+        semanticLvl?: 2 | 3;
+        visualLvl?: 2 | 3;
     }>(),
     {
         semanticLvl: 2,
         visualLvl: 2,
-    }
-)
-
+    },
+);
 </script>
 
 <template>
-    <SectionHeading
-        v-if="semanticLvl === 2"
-        :title :description
-        />
+    <SectionHeading v-if="semanticLvl === 2" :title :description :class />
     <SubSectionHeading
         v-else-if="semanticLvl === 3"
-        :title :description :class
+        :title
+        :description
+        :class
     />
 </template>
