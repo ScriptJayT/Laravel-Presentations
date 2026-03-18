93f2d0ebba7bba182b38462cac1fb91eb258d8e4
Jace <jt.scripter@gmail.com>
2026-03-18 11:42:58 +0100

    cleanup

--- resources/js/components/dashboard/models/NewLink.vue
+++ resources/js/components/dashboard/models/NewLink.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import IndexLink from '@/components/dashboard/models/IndexLink.vue';
 import { Plus } from 'lucide-vue-next';
-import { SectionHeading, SubSectionHeading, FalseHeading } from '@/components/global/text';
+import { cn } from '@/lib/utils';
+import { FalseHeading } from '@/components/global/text';
 import ActionModal from '../ActionModal.vue';
 
 const props = withDefaults(
@@ -11,21 +12,27 @@ const props = withDefaults(
     }>(),
     {
         headingLevel: 2,
-    }
+    },
 );
 </script>
 
 <template>
     <IndexLink :title="`<New ${title}>`" :unsemantic-title="true">
         <button
-            class="
-                model-create--button |
-                cursor-pointer
-                absolute inset-0
-                grid place-content-center
-                border-2 rounded-xl hover:border-green-700 focus-visible:border-green-700
-                outline-offset-8
-                "
+            :class="
+                cn(
+                    'model-create--button',
+                    'cursor-pointer',
+                    ['absolute', 'inset-0'],
+                    ['grid', 'place-content-center'],
+                    ['border-2', 'rounded-xl'],
+                    [
+                        'hover:border-green-700',
+                        'focus-visible:border-green-700',
+                    ],
+                    'outline-offset-8',
+                )
+            "
             :title="`Create a New ${title}`"
             command="show-modal"
             :commandfor="`newlink-modal-for-${title.toLowerCase()}`"
@@ -33,12 +40,15 @@ const props = withDefaults(
             <Plus class="size-[15cqw] opacity-15" />
         </button>
     </IndexLink>
-    <ActionModal :id="`newlink-modal-for-${title.toLowerCase()}`" class="min-w-[30vw] max-w-4xl">
+    <ActionModal
+        :id="`newlink-modal-for-${title.toLowerCase()}`"
+        class="max-w-4xl min-w-[30vw]"
+    >
         <FalseHeading
             :semantic-lvl="headingLevel"
             :title="`Create a new ${title}`"
             class="my-5"
         />
-        <slot/>
+        <slot />
     </ActionModal>
 </template>
