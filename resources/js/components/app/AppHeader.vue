7a25616333fcdcef6ccf0e3214cf5a2e442d55c9
Jace <jt.scripter@gmail.com>
2026-03-19 13:40:18 +0100

    ref appnavlist to own component

--- resources/js/components/app/AppHeader.vue
+++ resources/js/components/app/AppHeader.vue
@@ -1,34 +1,7 @@
 <script setup lang="ts">
-import { admin_presentation_index, login, home } from '@/routes';
-import { type RouteDefinition } from '@/wayfinder';
-import { safeQuery, getUser, cn } from '@/lib/utils';
-import { Link } from '@inertiajs/vue3';
-import UserDropDown from '@/components/app/UserDropDown.vue';
+import { safeQuery } from '@/lib/utils';
 import { AppLogo } from '../global/logo';
-import { useCurrentUrl } from '@/composables/useCurrentUrl';
-const user = getUser(true);
-type NavItem = {
-    title: string;
-    href: RouteDefinition<'get'>;
-    show: 'login' | 'guest' | 'always';
-};
-const links: NavItem[] = [
-    {
-        title: 'Home',
-        href: home(),
-        show: 'always',
-    },
-    {
-        title: 'Dashboard',
-        href: admin_presentation_index(),
-        show: 'login',
-    },
-    {
-        title: 'Log In',
-        href: login(),
-        show: 'guest',
-    },
-];
+import AppNavList from './AppNavList.vue';
 async function skipToContent(_e: MouseEvent) {
     const content = safeQuery('#site-content');
     if (!content) return;
@@ -36,7 +9,6 @@ async function skipToContent(_e: MouseEvent) {
     content.focus({ preventScroll: true });
     content.scrollIntoView({ behavior: 'smooth' });
 }
-const { isCurrentUrl } = useCurrentUrl();
 </script>
 
 <template>
@@ -59,73 +31,7 @@ const { isCurrentUrl } = useCurrentUrl();
                 <AppLogo />
             </div>
             <nav class="block w-fit min-w-fit select-none">
-                <ul
-                    class="flex flex-wrap items-center gap-4 text-sm leading-normal"
-                >
-                    <template v-for="_link in links">
-                        <li
-                            v-if="
-                                (_link.show == 'guest' && !user) ||
-                                (_link.show == 'login' && user) ||
-                                _link.show == 'always'
-                            "
-                            class="min-w-fit"
-                        >
-                            <Link
-                                :aria-current="
-                                    isCurrentUrl(_link.href) ? 'page' : false
-                                "
-                                :title="
-                                    isCurrentUrl(_link.href)
-                                        ? 'You are here'
-                                        : 'Go to'
-                                "
-                                :href="_link.href"
-                                v-text="_link.title"
-                                :class="
-                                    cn(
-                                        'block',
-                                        ['px-5', 'py-1.5'],
-                                        ['rounded-md', 'border-2'],
-                                        isCurrentUrl(_link.href)
-                                            ? [
-                                                  'border-amber-600',
-                                                  'dark:border-amber-500',
-                                              ]
-                                            : [
-                                                  [
-                                                      'border-neutral-500',
-                                                      'hover:border-cyan-600',
-                                                      'focus-visible:border-cyan-600',
-                                                  ],
-                                                  [
-                                                      'dark:border-cyan-900',
-                                                      'dark:hover:border-cyan-500',
-                                                      'dark:focus-visible:border-cyan-500',
-                                                  ],
-                                              ],
-                                        [
-                                            'outline-offset-6',
-                                            'focus-visible:outline-2',
-                                        ],
-                                        [
-                                            'hover:scale-105',
-                                            'focus-visible:scale-105',
-                                        ],
-                                        [
-                                            'bg-background',
-                                            'hover:bg-background/90',
-                                        ],
-                                        'transition-all',
-                                    )
-                                "
-                            />
-                        </li>
-                    </template>
-                    <li v-if="user" title="User menu">
-                        <UserDropDown />
-                    </li>
-                </ul>
+                <AppNavList />
             </nav>
         </div>
     </header>
---
+++ resources/js/components/app/AppNavList.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import UserDropDown from '@/components/app/UserDropDown.vue';
+import { useCurrentUrl } from '@/composables/useCurrentUrl';
+import { getUser, cn } from '@/lib/utils';
+import { admin_presentation_index, login, home } from '@/routes';
+import { type RouteDefinition } from '@/wayfinder';
+
+const user = getUser(true);
+const { isCurrentUrl } = useCurrentUrl();
+type NavItem = {
+    title: string;
+    href: RouteDefinition<'get'>;
+    show: 'login' | 'guest' | 'always';
+};
+const links: NavItem[] = [
+    {
+        title: 'Home',
+        href: home(),
+        show: 'always',
+    },
+    {
+        title: 'Dashboard',
+        href: admin_presentation_index(),
+        show: 'login',
+    },
+    {
+        title: 'Log In',
+        href: login(),
+        show: 'guest',
+    },
+];
+</script>
+
+<template>
+    <ul class="flex flex-wrap items-center gap-4 text-sm leading-normal">
+        <template v-for="_link in links">
+            <li
+                v-if="
+                    (_link.show == 'guest' && !user) ||
+                    (_link.show == 'login' && user) ||
+                    _link.show == 'always'
+                "
+                class="min-w-fit"
+            >
+                <Link
+                    :aria-current="isCurrentUrl(_link.href) ? 'page' : false"
+                    :title="isCurrentUrl(_link.href) ? 'You are here' : 'Go to'"
+                    :href="_link.href"
+                    v-text="_link.title"
+                    :class="
+                        cn(
+                            'block',
+                            ['px-5', 'py-1.5'],
+                            ['rounded-md', 'border-2'],
+                            isCurrentUrl(_link.href)
+                                ? ['border-amber-600', 'dark:border-amber-500']
+                                : [
+                                      [
+                                          'border-neutral-500',
+                                          'hover:border-cyan-600',
+                                          'focus-visible:border-cyan-600',
+                                      ],
+                                      [
+                                          'dark:border-cyan-900',
+                                          'dark:hover:border-cyan-500',
+                                          'dark:focus-visible:border-cyan-500',
+                                      ],
+                                  ],
+                            ['outline-offset-6', 'focus-visible:outline-2'],
+                            ['hover:scale-105', 'focus-visible:scale-105'],
+                            ['bg-background', 'hover:bg-background/90'],
+                            'transition-all',
+                        )
+                    "
+                />
+            </li>
+        </template>
+        <li v-if="user" title="User menu">
+            <UserDropDown />
+        </li>
+    </ul>
+</template>
