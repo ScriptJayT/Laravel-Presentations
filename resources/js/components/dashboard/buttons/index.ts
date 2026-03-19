b824994ad45656e81b88b4ce723386ff4fe2f090
Jace <jt.scripter@gmail.com>
2026-03-19 10:21:45 +0100

    ref button styles

--- resources/js/components/dashboard/buttons/index.ts
+++ resources/js/components/dashboard/buttons/index.ts
@@ -1,26 +1,39 @@
+import { cn } from '@/lib/utils';
+
 export {default as DoubleIconLink} from './DoubleIconLink.vue';
 export {default as ActionLink} from './ActionLink.vue';
 export {default as ActionButton} from './ActionButton.vue';
 
-export const actionButtonClasses = () => [
-    'cursor-pointer', 'disabled:cursor-not-allowed',
+const cursorStyles = [
+    'cursor-pointer',
+    'disabled:cursor-not-allowed',
     'select-none',
+];
+const interactionStyles = [
+    'outline-color-ring',
+    'outline-offset-4', 'focus-visible:outline-2',
+    'hover:bg-accent', 'dark:hover:bg-accent/50',
+    'disabled:opacity-50',
+    'transition-colors',
+];
+const borderStyles = [
+    'border', 'rounded-md',
+];
+
+export const actionButtonClasses = () => cn(
+    cursorStyles,
+    interactionStyles,
+    borderStyles,
     'block', 'w-fit',
     'px-3', 'py-2',
-    'border', 'rounded-md',
-    'outline-offset-4', 'focus-visible:outline-2',
-    'disabled:opacity-50',
-    'hover:bg-accent', 'dark:hover:bg-accent/50',
-    'transition-colors',
-];
-export const formfieldButtonClasses = () => [
+);
+export const formfieldButtonClasses = () => cn(
+    cursorStyles,
+    interactionStyles,
+    borderStyles,
     'min-w-fit', 'p-4',
-    'border-2', 'rounded-md',
-    'outline-offset-4',
     'text-center', 'text-balance',
-    'hover:bg-accent', 'dark:hover:bg-accent/50',
-    'transition-colors',
-];
+);
 export const doubleIconButtonClasses = () => [
     'flex',
     'gap-3',
