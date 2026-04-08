a08a4641325fb9b09c24a7312db18894119751ff
Jace <jt.scripter@gmail.com>
2026-04-08 23:39:00 +0200

    added readonly formfield

---
+++ resources/js/components/global/form/fields/ReadonlyField.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { TextField } from '..';
+defineProps<{
+    label: string;
+    value?: string|number;
+}>();
+</script>
+
+<template>
+    <TextField name="--" :label :value="`${value}`" :disabled="true" />
+</template>
