b041d7e0a35c41ce7ff48e82699b7f00e631da2f
Jace <jt.scripter@gmail.com>
2026-04-09 00:54:33 +0200

    styling user edit form

--- resources/js/components/dashboard/forms/EditUser.vue
+++ resources/js/components/dashboard/forms/EditUser.vue
@@ -11,28 +11,21 @@ defineProps<{ user: User }>();
         id="update-form-edit"
         :send-to="update.form(user.id)"
         :show-button="false"
-        class="grid grid-cols-2 gap-10"
+        class="space-y-5"
     >
-        <fieldset class="space-y-5">
-            <legend class="sr-only">Credentials</legend>
-            <TextField label="Name:" name="name" :value="user.name" />
-            <TextField label="Email:" name="email" :value="user.email" />
-            <TextField
-                label="Email validated:"
-                name="--"
-                :value="user.email_verified_at ? 'Yes' : 'No'"
-                disabled
-            />
-        </fieldset>
         <fieldset class="space-y-5">
             <legend class="sr-only">Visual Representation</legend>
             <UserAvatar
                 :avatar="user.avatar"
                 :user-name="user.name"
                 :inline="false"
-                class="ml-auto"
             />
             <FileField label="Avatar:" name="avatar" />
+        </fieldset>
+        <fieldset class="space-y-5">
+            <legend class="sr-only">Credentials</legend>
+            <TextField label="Name:" name="name" :value="user.name" />
+            <TextField label="Email:" name="email" :value="user.email" />
         </fieldset>
     </Form>
 </template>
