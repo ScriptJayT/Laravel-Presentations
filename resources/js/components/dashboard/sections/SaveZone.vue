13eb50aae21516e83e6a4b6e47a5032d5894c734
Jace <jt.scripter@gmail.com>
2026-04-15 12:11:07 +0200

    added ctrl+s function to savezone

--- resources/js/components/dashboard/sections/SaveZone.vue
+++ resources/js/components/dashboard/sections/SaveZone.vue
@@ -1,22 +1,54 @@
 <script setup lang="ts" >
 import { Save } from 'lucide-vue-next';
+import { onMounted, onUnmounted, useId } from 'vue';
+import { cn, safeQuery } from '@/lib/utils';
 import AsideZone from './AsideZone.vue';
 import { metaButtonClasses, metaButtonSpanClasses } from '@/components/dashboard/sections';
-import { cn } from '@/lib/utils';
 
-defineProps<{
-    formId: string;
-}>();
+defineProps<{ formId: string }>();
+const btnId = useId();
+const signal = new AbortController();
+onMounted(() => {
+    const btn = safeQuery(`#${btnId}`);
+    document.addEventListener('keydown', (_e: KeyboardEvent) => {
+        if(!btn) return;
+        if(_e.key === "s" && _e.ctrlKey) {
+            _e.preventDefault();
+            _e.stopImmediatePropagation();
+            _e.stopPropagation();
+
+            btn.click();
+        }
+    }, {signal: signal.signal});
+});
+onUnmounted(() => {
+    console.log("[DEV] abort ctrl+s function");
+    signal.abort();
+});
 </script>
 
 <template>
     <AsideZone>
         <button
+            :id="btnId"
             :form="formId"
             :class="cn(metaButtonClasses(), 'w-full')"
         >
-            <span :class="cn(metaButtonSpanClasses())"> Save </span>
-            <Save class="size-4"/>
+            <span :class="cn(metaButtonSpanClasses())">
+                Save <kbd class="text-sm text-muted-foreground uppercase">ctrl+s</kbd>
+            </span>
+            <Save class="size-4" />
         </button>
     </AsideZone>
 </template>
+
+<style scoped>
+    kbd {
+        &::before {
+            content: "[";
+        }
+        &::after {
+            content: "]";
+        }
+    }
+</style>
