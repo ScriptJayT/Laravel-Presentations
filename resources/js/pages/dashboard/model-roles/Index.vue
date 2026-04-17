21591f8ca8b5e7c6fbf95142b15653e4fd2c054f
Jace <jt.scripter@gmail.com>
2026-04-17 13:49:52 +0200

    start new model views

---
+++ resources/js/pages/dashboard/model-roles/Index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { type Role, type BreadcrumbItem } from '@/types';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Container } from '@/components/dashboard/containers';
+
+const props = defineProps<{ allRoles: Role[] }>();
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Users',
+    },
+    {
+        title: 'Roles',
+    },
+];
+</script>
+
+<template>
+    <AppLayout meta-title="Roles & Permissions" :breadcrumbs="breadcrumbs">
+        <Container
+            title="Roles & Permissions"
+        />
+    </AppLayout>
+</template>
